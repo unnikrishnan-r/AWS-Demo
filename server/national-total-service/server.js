df836ebd31f0b194cbee59163b21b187ee28f31d
Judy <judys717@hotmail.com>
2020-05-20 05:45:45 -0400

    update

--- server/national-total-service/server.js
+++ server/national-total-service/server.js
@@ -5,6 +5,6 @@ app.get("/api/national/", (req, res) => {
   res.send("Hello world from a National Total Service!");
 });
 
-app.listen(3001, () => {
-  console.log("Server is up on 3001");
+app.listen(3003, () => {
+  console.log("Server is up on 3003");
 });
