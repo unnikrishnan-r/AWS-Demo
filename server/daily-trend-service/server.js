e958da614a5289f2a911ea17ad3f6226017b572f
Judy <judys717@hotmail.com>
2020-05-20 05:45:13 -0400

    update

--- server/daily-trend-service/server.js
+++ server/daily-trend-service/server.js
@@ -5,6 +5,6 @@ app.get("/api/daily/", (req, res) => {
   res.send("Hello world from a Daily Trend Service!");
 });
 
-app.listen(3001, () => {
-  console.log("Server is up on 3001");
+app.listen(3002, () => {
+  console.log("Server is up on 3002");
 });
