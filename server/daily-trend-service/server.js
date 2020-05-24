078220d34c5e32c5cf48a4557b9040fc07905bfd
Judy <judys717@hotmail.com>
2020-05-24 05:32:38 -0400

    update '

--- server/daily-trend-service/server.js
+++ server/daily-trend-service/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/api/daily", (req, res) => {
-  res.status(200).send("Hello world from a Daily Trend Service!");
+  res.status(200).json({ message: "Hello world from a Daily Trend Service!" });
 });
 
 app.listen(3002, () => {
--- server/national-total-service/server.js
+++ server/national-total-service/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.status(200).send("Hello world from a National Total Service!");
+app.get("/api/national", (req, res) => {
+  res
+    .status(200)
+    .json({ message: "Hello world from a National Trend Service!" });
 });
 
 app.listen(3003, () => {
